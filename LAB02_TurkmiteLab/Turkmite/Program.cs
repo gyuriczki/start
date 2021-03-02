0022e2b064ad93607e24617e08bf257a41a44577
jojartbence <jojart.bence96@gmail.com>
2021-03-02 20:31:50 +0100

    Created base turkmite class so turkmites with customized step rules can be created

--- LAB02_TurkmiteLab/Turkmite/Program.cs
+++ LAB02_TurkmiteLab/Turkmite/Program.cs
@@ -8,7 +8,7 @@ namespace TurkMite
         static void Main()
         {
             Mat img = new Mat(200, 200, MatType.CV_8UC3, new Scalar(0, 0, 0));
-            var turkmite = new TurkMite(img);
+            var turkmite = new OriginalTurkmite(img);
             for (int i = 0; i < 13000; i++)
             {
                 turkmite.Step();
@@ -17,42 +17,16 @@ namespace TurkMite
             Cv2.WaitKey();
         }
 
-        class TurkMite
+        class OriginalTurkmite : TurkmiteBase
         {
-            public Mat Image { get; }
-            private int x;
-            private int y;
-            private int direction;  // 0 up, 1 right, 2 down, 3 left
-            private Mat.Indexer<Vec3b> indexer;
-            public TurkMite(Mat image)
+            readonly private Vec3b black = new Vec3b(0, 0, 0);
+            readonly private Vec3b white = new Vec3b(255, 255, 255);
+
+            public OriginalTurkmite(Mat image) : base(image)
             {
-                Image = image;
-                x = image.Cols / 2;
-                y = image.Rows / 2;
-                direction = 0;
-                indexer = image.GetGenericIndexer<Vec3b>();
             }
 
-            readonly Vec3b black = new Vec3b(0, 0, 0);
-            readonly Vec3b white = new Vec3b(255, 255, 255);
-
-            public void Step()
-            {
-                indexer[y, x] = GetNextColorAndUpdateDirection(indexer[y, x]);
-                PerformMove();
-            }
-
-            private void PerformMove()
-            {
-                direction = (direction + 4) % 4;
-                var delta = new (int x, int y)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
-                x += delta[direction].x;
-                y += delta[direction].y;
-                x = Math.Max(0, Math.Min(Image.Cols, x));
-                y = Math.Max(0, Math.Min(Image.Rows, y));
-            }
-
-            private Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
+            protected override Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
             {
                 if (currentColor == black)
                 {
@@ -66,5 +40,42 @@ namespace TurkMite
                 }
             }
         }
+
+        abstract class TurkmiteBase
+        {
+            public Mat Image { get; }
+            private Mat.Indexer<Vec3b> indexer;
+            private int x;
+            private int y;
+            protected int direction;  // 0 up, 1 right, 2 down, 3 left
+            public TurkmiteBase(Mat image)
+            {
+                Image = image;
+                x = image.Cols / 2;
+                y = image.Rows / 2;
+                direction = 0;
+                indexer = image.GetGenericIndexer<Vec3b>();
+            }
+
+            readonly private (int x, int y)[] delta = new (int x, int y)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
+
+            public void Step()
+            {
+                indexer[y, x] = GetNextColorAndUpdateDirection(indexer[y, x]);
+                PerformMove();
+            }
+
+            private void PerformMove()
+            {
+                direction = (direction + 4) % 4;
+                x += delta[direction].x;
+                y += delta[direction].y;
+                x = Math.Max(0, Math.Min(Image.Cols, x));
+                y = Math.Max(0, Math.Min(Image.Rows, y));
+            }
+
+            protected abstract Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor);
+        }
+
     }
 }
