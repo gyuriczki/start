05bed9871957ed439cc46b9591a46d57dddaf47e
gyuriczki <gyuriczki@gmail.com>
2021-03-12 16:25:10 +0100

    Task4 Add diagonal move

--- LAB10_AttaxxPlus/AttaxxPlus/Model/Operations/CloneMoveOperation.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/Model/Operations/CloneMoveOperation.cs
@@ -15,8 +15,8 @@ namespace AttaxxPlus.Model.Operations
 
             // Note: selectedField is always the players own field...
             // EVIP: IsEmpty() is more descriptive than "Owner == 0"
-            if (Math.Abs(selectedField.Row - currentField.Row)
-                + Math.Abs(selectedField.Column - currentField.Column) == 1
+            if ((Math.Abs(selectedField.Row - currentField.Row) <= 1)
+                && (Math.Abs(selectedField.Column - currentField.Column) <= 1)
                 && !selectedField.IsEmpty()
                 && currentField.IsEmpty())
             {
